bc0814849ca7505f94ac6f6c8ba688b5ec46f581
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2026-02-19 15:21:19 +0100

    Remove stray src folder from repo root

--- src/modules/bands/dto/update-band.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateBandDto } from './create-band.dto';
-
-/**
- * DTO for updating a band
- */
-export class UpdateBandDto extends PartialType(CreateBandDto) {}
-
--- src/modules/members/dto/update-member.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateMemberDto } from './create-member.dto';
-
-/**
- * DTO for updating a member
- * All fields are optional
- */
-export class UpdateMemberDto extends PartialType(CreateMemberDto) {}
-
